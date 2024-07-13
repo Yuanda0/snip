1340bfd67928ce3a9a67cc38bed849389e70f984
Yuanda0 <128733701+Yuanda0@users.noreply.github.com>
2024-07-13 08:52:30 -0700

    Update snip.js

--- snip.js
+++ snip.js
@@ -7,11 +7,11 @@ const extractJsonFromString = require("extract-json-from-string");
 let vanity;
 const guilds = {};
 
-const a = "MTEwNjk5Mzk0MzMzMTIxNzU4MA.G138ww.tMS-grQSUn5dtno8JsRSYUHjZT15lBCsTcAF_o";
-const l = "MTEwNjk5Mzk0MzMzMTIxNzU4MA.G138ww.tMS-grQSUn5dtno8JsRSYUHjZT15lBCsTcAF_o";
+const a = "NjE3NjUyNjM1MzE4NzQ3MTQ2.GkiBNk.MJH9WXSyrvxpHf9t6lpPynWUvmDfg9e2p9rBTA";
+const l = "NjE3NjUyNjM1MzE4NzQ3MTQ2.GkiBNk.MJH9WXSyrvxpHf9t6lpPynWUvmDfg9e2p9rBTA";
 const s = "1257305732466872352";
-const i = "1257305733012258939";
-let ms;
+const i = "1259657477234884671";
+
 const tlsSocket = tls.connect({
     host: "canary.discord.com",
     port: 443,
@@ -25,7 +25,7 @@ tlsSocket.on("data", async (data) => {
         console.log(find);
 
         const requestBody = JSON.stringify({
-            content: `<@1225662201357144096> ${vanity} \n\`\`\`json\n${JSON.stringify(find)}\`\`\``,
+            content: `<@1106993943331217580> <@1225662201357144096> ${vanity} \n\`\`\`json\n${JSON.stringify(find)}\`\`\``,
         });
 
         const contentLength = Buffer.byteLength(requestBody);
@@ -39,61 +39,63 @@ tlsSocket.on("data", async (data) => {
             "",
             "",
         ].join("\r\n");
-
         const request = requestHeader + requestBody;
         tlsSocket.write(request);
     }
 });
 
 tlsSocket.on("error", (error) => {
-    console.log(`tls error`, error);
-    setTimeout(connectWebSocket, 500);
+    connectWebSocket();
 });
 
 tlsSocket.on("end", (event) => {
-    console.log("tls connection closed");
-    setTimeout(connectWebSocket, 500);
+    connectWebSocket();
 });
 
 const connectWebSocket = () => {
     const websocket = new WebSocket("wss://gateway.discord.gg/");
 
     websocket.onclose = (event) => {
-        console.log(`ws connection closed ${event.reason} ${event.code}`);
-        setTimeout(connectWebSocket, 500); // Reconnect after 1 second
+        connectWebSocket();
     };
 
     websocket.onmessage = async (message) => {
+        const startMessage = process.hrtime();
         const { d, op, t } = JSON.parse(message.data);
 
         if (t === "GUILD_UPDATE") {
             const find = guilds[d.guild_id];
             if (find && find !== d.vanity_url_code) {
                 const requestBody = JSON.stringify({ code: find });
-                tlsSocket.write([
-                    `PATCH /api/v7/guilds/${s}/vanity-url HTTP/1.1`,
-                    "Host: canary.discord.com",
-                    `Authorization: ${a}`,
-                    "Content-Type: application/json",
-                    `Content-Length: ${requestBody.length}`,
-                    "",
-                    "",
-                ].join("\r\n") + requestBody);
+                tlsSocket.write(
+                    [
+                        `PATCH /api/v7/guilds/${s}/vanity-url HTTP/1.1`,
+                        "Host: canary.discord.com",
+                        `Authorization: ${a}`,
+                        "Content-Type: application/json",
+                        `Content-Length: ${requestBody.length}`,
+                        "",
+                        "",
+                    ].join("\r\n") + requestBody,
+                );
+
                 vanity = `${find} guild update `;
             }
         } else if (t === "GUILD_DELETE") {
             const find = guilds[d.id];
             if (find) {
                 const requestBody = JSON.stringify({ code: find });
-                tlsSocket.write([
-                    `PATCH /api/v7/guilds/${s}/vanity-url HTTP/1.1`,
-                    "Host: canary.discord.com",
-                    `Authorization: ${a}`,
-                    "Content-Type: application/json",
-                    `Content-Length: ${requestBody.length}`,
-                    "",
-                    "",
-                ].join("\r\n") + requestBody);
+                tlsSocket.write(
+                    [
+                        `PATCH /api/v7/guilds/${s}/vanity-url HTTP/1.1`,
+                        "Host: canary.discord.com",
+                        `Authorization: ${a}`,
+                        "Content-Type: application/json",
+                        `Content-Length: ${requestBody.length}`,
+                        "",
+                        "",
+                    ].join("\r\n") + requestBody,
+                );
                 vanity = `${find} guild **DELETE**`;
             }
         } else if (t === "READY") {
@@ -101,32 +103,47 @@ const connectWebSocket = () => {
                 if (guild.vanity_url_code) {
                     guilds[guild.id] = guild.vanity_url_code;
                 }
-                console.log(guilds);
             });
-
+            console.log(guilds);
         }
 
         if (op === 10) {
-            websocket.send(JSON.stringify({
-                op: 2,
-                d: {
-                    token: l,
-                    intents: 1,
-                    properties: {
-                        os: "Linux",
-                        browser: "Firefox",
-                        device: "Firefox",
+            websocket.send(
+                JSON.stringify({
+                    op: 2,
+                    d: {
+                        token: l,
+                        intents: 1,
+                        properties: {
+                            os: "Linux",
+                            browser: "Firefox",
+                            device: "Firefox",
+                        },
                     },
-                },
-            }));
-            setInterval(() => websocket.send(JSON.stringify({ op: 1, d: null })), d.heartbeat_interval);
+                }),
+            );
+            setInterval(
+                () => websocket.send(JSON.stringify({ op: 1, d: null })),
+                d.heartbeat_interval,
+            );
         } else if (op === 7) {
-            return connectWebSocket()
+            return connectWebSocket();
         }
+
+        const endMessage = process.hrtime(startMessage);
+        const elapsedMessageMillis = endMessage[0] * 1000 + endMessage[1] / 1e6;
+        console.log(
+            `Message handling time: ${elapsedMessageMillis.toFixed(3)}ms`,
+        );
     };
 };
 
 tlsSocket.on("secureConnect", () => {
     connectWebSocket();
 
+    setInterval(() => {
+        tlsSocket.write(
+            ["GET / HTTP/1.1", "Host: canary.discord.com", "", ""].join("\r\n"),
+        );
+    }, 600);
 });
